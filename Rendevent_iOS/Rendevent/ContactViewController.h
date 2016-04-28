479b8b5a5c5a076a790632cb2083cd6805bb22d3
liaoyg <liaoyg620@163.com>
2016-04-27 23:31:01 -0700

    1.2
    Implement Contact Part

---
+++ Rendevent_iOS/Rendevent/ContactViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContactViewController.h
+//  Rendevent
+//
+//  Created by LiaoYangguang on 4/27/16.
+//  Copyright © 2016 LiaoYangguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ChineseSorting/ChineseString.h"
+
+@interface ContactViewController : UITableViewController
+@property(nonatomic,strong)NSMutableArray *indexArray;
+@property(nonatomic,strong)NSMutableArray *letterResultArr;
+@end
