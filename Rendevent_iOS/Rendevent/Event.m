492b1860fe05a690e8a7afccfc100f34b1fbc1f9
liaoyg <liaoyg620@163.com>
2016-05-04 20:24:18 -0700

    2.1
    Add achieve function

--- Rendevent_iOS/Rendevent/Event.m
+++ Rendevent_iOS/Rendevent/Event.m
@@ -23,4 +23,44 @@
     return event;
 }
 
+- (void)encodeWithCoder:(NSCoder *)coder
+{
+    [coder encodeObject:self.name forKey:@"name"];
+    [coder encodeInteger:self.category forKey:@"category"];
+    [coder encodeInteger:self.event_Id forKey:@"event_Id"];
+    [coder encodeObject:self.descrip forKey:@"descrip"];
+    [coder encodeObject:self.startTime forKey:@"startTime"];
+    [coder encodeObject:self.endTime forKey:@"endTime"];
+    [coder encodeObject:self.members forKey:@"members"];
+    [coder encodeInteger:self.minimum forKey:@"minimum"];
+    [coder encodeInteger:self.maximum forKey:@"maximum"];
+    [coder encodeInteger:self.mini_age forKey:@"mini_age"];
+    [coder encodeInteger:self.max_age forKey:@"max_age"];
+    [coder encodeInteger:self.host_Id forKey:@"max_age"];
+    [coder encodeInteger:self.message_group_Id forKey:@"max_age"];
+    [coder encodeObject:[NSNumber numberWithInteger:self.status] forKey:@"status"];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super init];
+    if (self) {
+        self.category = [coder decodeIntegerForKey:@"category"];
+        self.name = [coder decodeObjectForKey:@"name"];
+        self.event_Id = [coder decodeIntegerForKey:@"event_Id"];
+        self.descrip = [coder decodeObjectForKey:@"descrip"];
+        self.startTime = [coder decodeObjectForKey:@"startTime"];
+        self.endTime = [coder decodeObjectForKey:@"endTime"];
+        self.members = [coder decodeObjectForKey:@"members"];
+        self.minimum = [coder decodeIntegerForKey:@"minimum"];
+        self.maximum = [coder decodeIntegerForKey:@"maximum"];
+        self.mini_age = [coder decodeIntegerForKey:@"mini_age"];
+        self.max_age = [coder decodeIntegerForKey:@"max_age"];
+        self.host_Id = [coder decodeIntegerForKey:@"host_Id"];
+        self.message_group_Id = [coder decodeIntegerForKey:@"message_group_Id"];
+        self.status = [[coder decodeObjectForKey:@"status"] intValue];
+    }
+    return self;
+}
+
 @end
--- Rendevent_iOS/Rendevent/RecomEventsController.m
+++ Rendevent_iOS/Rendevent/RecomEventsController.m
@@ -58,17 +58,23 @@
     //initialize data modal
     currentCategoryIndex = 0;
     
-    eventsList = [NSMutableArray arrayWithCapacity:10];
-    Event *basketball = [Event initWithName:@"PlayBasketBall" WithCategory:1 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
-    [eventsList addObject:basketball];
-    Event *ktv = [Event initWithName:@"KTV" WithCategory:2 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
-    [eventsList addObject:ktv];
-    Event *hunan = [Event initWithName:@"Hunan" WithCategory:3 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
-    [eventsList addObject:hunan];
-    Event *hike = [Event initWithName:@"Hiking!" WithCategory:4 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
-    [eventsList addObject:hike];
-    Event *football = [Event initWithName:@"PlayFootball" WithCategory:1 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
-    [eventsList addObject:football];
+//    eventsList = [NSMutableArray arrayWithCapacity:10];
+//    Event *basketball = [Event initWithName:@"PlayBasketBall" WithCategory:1 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
+//    [eventsList addObject:basketball];
+//    Event *ktv = [Event initWithName:@"KTV" WithCategory:2 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
+//    [eventsList addObject:ktv];
+//    Event *hunan = [Event initWithName:@"Hunan" WithCategory:3 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
+//    [eventsList addObject:hunan];
+//    Event *hike = [Event initWithName:@"Hiking!" WithCategory:4 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
+//    [eventsList addObject:hike];
+//    Event *football = [Event initWithName:@"PlayFootball" WithCategory:1 WithDes:@"" WithStartTime:[NSDate date] WithEndTime:[NSDate date]];
+//    [eventsList addObject:football];
+    
+    //unarchieve data
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *appFile = [documentsDirectory stringByAppendingPathComponent:@"EventList.ldata"];
+    eventsList = [NSKeyedUnarchiver unarchiveObjectWithFile:appFile];
     
     cureventsList = [self filteringEventList:eventsList withCategory:currentCategoryIndex];
     
@@ -82,7 +88,7 @@
 -(NSMutableArray *)filteringEventList:(NSMutableArray*)eventList withCategory:(int)caIndex
 {
     NSMutableArray *newList = [NSMutableArray arrayWithCapacity:10];
-    for (Event* e in eventsList) {
+    for (Event* e in eventList) {
         if(e.category == caIndex || caIndex == 0)
             [newList addObject:e];
     }
